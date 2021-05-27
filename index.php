4bd3d06fca19fc7542f1b2d86d3dc4b832460edc
antar <toobrokeforrysll@gmail.com>
2021-05-27 10:40:33 +0200

    feat: index.php changes for footer and header includes

--- index.php
+++ index.php
@@ -1,19 +1,41 @@
 <?php
 
 include_once("includes/dbh.inc.php");
+include_once("header.php")
 
 ?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title></title>
-	</head>
-	<body>
-		<?php
-		$object = new Dbh;
-		$object->connect();
-		?>
-	</body>
-</html>
+<div id="container">
+	  <div class="inner">
+		<div id="content"> 
+		  
+		  <div id="home" class="content-region hide">
+			<h2>Home</h2>
+			<p>
+			  Some Guys have no clue, what they are doing at their job. <a href="https://wes.fm">Love</a>
+			</p>
+		  </div>
+		  
+		  <div id="about" class="content-region hide">
+			<h2>About</h2>
+			<p>
+			  Sed tempus urna et pharetra pharetra massa massa ultricies. Porttitor rhoncus dolor purus non enim praesent. Id aliquet risus feugiat in ante metus dictum. 
+			</p>
+		  </div>
+		  
+		  <div id="contact" class="content-region hide">
+			<h2>Contact</h2>
+			<p>
+			  Pellentesque nec nam aliquam sem et tortor consequat. Quisque sagittis purus sit amet volutpat consequat. Magna eget est lorem ipsum dolor. 
+			</p>
+		  </div>
+		  
+		</div>
+	  </div>
+</div>
+
+<?php
+
+include_once("footer.php");
+
+?>
