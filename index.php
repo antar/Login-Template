4c53ca6a1083ed082412f6324aedc6a8345dd7d9
antar <toobrokeforrysll@gmail.com>
2021-05-27 11:20:01 +0200

    feat: signup and login forms

--- index.php
+++ index.php
@@ -8,28 +8,66 @@ include_once("header.php")
 <div id="container">
 	  <div class="inner">
 		<div id="content"> 
-		  
 		  <div id="home" class="content-region hide">
 			<h2>Home</h2>
 			<p>
 			  Some Guys have no clue, what they are doing at their job. <a href="https://wes.fm">Love</a>
 			</p>
 		  </div>
-		  
-		  <div id="about" class="content-region hide">
-			<h2>About</h2>
-			<p>
-			  Sed tempus urna et pharetra pharetra massa massa ultricies. Porttitor rhoncus dolor purus non enim praesent. Id aliquet risus feugiat in ante metus dictum. 
-			</p>
+		</div>
+	  </div>
+</div>
+
+<style>
+input  {
+	  margin: 20px !important;
+	  width: 50% !important;
+	  height: 40px !important;
+	}
+button {
+	  margin: 20px !important;
+	  width: 51% !important;
+	  height: 40px !important;
+	  cursor: pointer;
+}
+</style>
+
+<div id="container">
+	  <div class="inner">
+		<div id="content"> 
+		  <div id="signup" class="content-region hide">
+			  <h2>Please enter the fields bellow</h2>
+			<form action="signup.inc.php" method="post">
+				<input type="text" name="name" class="input-class" placeholder="Full name..." />
+				<br>
+				<input type="email" name="email" class="input-class" placeholder="Email..." />
+				<br>
+				<input type="text" name="uid" class="input-class" placeholder="Username..." />
+				<br>
+				<input type="password" name="pwd" class="input-class" placeholder="Password..." />
+				<br>
+				<input type="password" name="pwdrepeat" class="input-class" placeholder="Repeat password..." />
+				<br>
+				<button type="submit" class="input-class" name="submit">Sign Up </button>
+			</form>
 		  </div>
-		  
-		  <div id="contact" class="content-region hide">
-			<h2>Contact</h2>
-			<p>
-			  Pellentesque nec nam aliquam sem et tortor consequat. Quisque sagittis purus sit amet volutpat consequat. Magna eget est lorem ipsum dolor. 
-			</p>
+		</div>
+	  </div>
+</div>
+
+<div id="container">
+	  <div class="inner">
+		<div id="content"> 
+		  <div id="login" class="content-region hide">
+			  <h2>Please enter the fields bellow</h2>
+			<form action="login.inc.php" method="post">
+				<input type="text" name="name" class="input-class" placeholder="Username/Email..." />
+				<br>
+				<input type="password" name="pwd" class="input-class" placeholder="Password..." />
+				<br>
+				<button type="submit" class="input-class" name="submit">Log In</button>
+			</form>
 		  </div>
-		  
 		</div>
 	  </div>
 </div>
